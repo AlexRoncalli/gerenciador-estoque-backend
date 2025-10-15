346cd73839146ba5b7bcbf6bc37d6aab816cac4d
AlexanderRoncalli <42423882+AlexRoncalli@users.noreply.github.com>
2025-10-15 12:22:28 -0300

    feat: adiciona tratador de erros global para depuração

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,4 @@
-// em src/server.ts
-import * as dotenv from 'dotenv';
-dotenv.config();
+// Em: src/server.ts
 
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
@@ -8,16 +6,33 @@ import { routes } from './routes.js';
 
 const app = Fastify({ logger: true });
 
-const start = async () => {
-  // 1. REGISTRE O CORS PRIMEIRO!
-  await app.register(cors);
+// --- INÍCIO DO TRATAMENTO DE ERROS GLOBAL ---
 
-  // 2. DEPOIS REGISTRE AS ROTAS
+app.setErrorHandler((error, request, reply) => {
+  // Loga o erro completo no console do Render para vermos o que é
+  app.log.error(error);
+
+  // Envia uma resposta genérica para o frontend
+  reply.status(500).send({ message: 'Ocorreu um erro interno no servidor.' });
+});
+
+// --- FIM DO TRATAMENTO DE ERROS GLOBAL ---
+
+
+const start = async () => {
+  // Registra o CORS primeiro
+  await app.register(cors, {
+    origin: "https://frontestoque.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  });
+
+  // Depois registra as rotas
   await app.register(routes);
 
   try {
     const port = process.env.PORT ? Number(process.env.PORT) : 3333;
     await app.listen({ port: port, host: '0.0.0.0' });
+
   } catch (err) {
     app.log.error(err);
     process.exit(1);
