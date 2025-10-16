4e66acf5b6c1096db5272657371b0511ee587417
AlexanderRoncalli <42423882+AlexRoncalli@users.noreply.github.com>
2025-10-16 11:39:08 -0300

    ordem decrescente de exits e locations

--- src/services/ListExitsService.ts
+++ src/services/ListExitsService.ts
@@ -2,7 +2,11 @@ import { prisma } from "../prisma/index.js";
 
 class ListExitsService {
   async execute() {
-    const exits = await prisma.productExit.findMany();
+    const exits = await prisma.productExit.findMany({
+      orderBy: {
+        create_at: 'desc' // <-- A CORREÇÃO
+      }
+    });
     return exits;
   }
 }
--- src/services/ListLocationsService.ts
+++ src/services/ListLocationsService.ts
@@ -2,7 +2,11 @@ import { prisma } from "../prisma/index.js";
 
 class ListLocationsService {
   async execute() {
-    const locations = await prisma.productLocation.findMany();
+    const locations = await prisma.productLocation.findMany({
+      orderBy: {
+        create_at: 'desc' // <-- A CORREÇÃO
+      }
+    });
     return locations;
   }
 }
