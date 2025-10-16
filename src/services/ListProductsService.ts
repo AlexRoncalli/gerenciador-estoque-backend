4035bb7fbced0e9ef12d3bf115850b655cb99d1a
AlexanderRoncalli <42423882+AlexRoncalli@users.noreply.github.com>
2025-10-16 11:34:15 -0300

    Comit para ordenar em ordem decrescente a lista de produtors

--- src/services/ListProductsService.ts
+++ src/services/ListProductsService.ts
@@ -1,12 +1,14 @@
 import { prisma } from "../prisma/index.js";
 
-class ListProductsService{
-    async execute(){
-
-        const products = await prisma.product.findMany()
-
-        return products;
-    }
+class ListProductsService {
+  async execute() {
+    const products = await prisma.product.findMany({
+      orderBy: {
+        create_at: 'desc' // Ordena pelo campo 'create_at' em ordem decrescente
+      }
+    });
+    return products;
+  }
 }
 
-export { ListProductsService }
+export { ListProductsService };
